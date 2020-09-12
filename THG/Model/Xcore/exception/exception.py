2053286edaadf1729e9434252189b50ed072bee3
darkcode0x00 <darkcode357@gmail.com>
2020-09-12 20:57:50 -0300

    THG-48 [core][opt_address]

---
+++ THG/Model/Xcore/exception/exception.py
@@ -0,0 +1,17 @@
+##
+# This module requires THG: https://darkcode0x00.com/download
+# Current source: https://github.com/darkcode357/thg-framework
+##
+"""
+This library is used to manager exception
+"""
+__version_exception__ = '1.0'
+
+
+class AddressValueError(ValueError):
+    """A Value Error related to the address"""
+
+class NetmaskValueError(ValueError):
+    """
+    A value Error related to the address
+    """
---
+++ THG/Model/Xcore/ipaddress.py
@@ -0,0 +1,80 @@
+##
+# This module requires THG: https://darkcode0x00.com/download
+# Current source: https://github.com/darkcode357/thg-framework
+##
+"""
+This library is used to  create/poke/manipulate IPv4 and IPv6 addresses and networks
+"""
+__version__ = '1.1'
+
+import functools
+import random
+import re
+import os
+import importlib
+import string
+import random
+from functools import wraps
+import string
+IPV4LENGTH = 32
+IPV6LENGTH = 128
+
+def random_text(length: int, alph: str = string.ascii_letters + string.digits) -> str:
+    """ Generates random string text
+    :param int length: length of text to generate
+    :param str alph: string of all possible characters to choose from
+    :return str: generated random string of specified size
+    """
+
+    return "".join(random.choice(alph) for _ in range(length))
+
+
+def is_ipv4(address: str) -> bool:
+    """ Checks if given address is valid IPv4 address
+    :param str address: IP address to check
+    :return bool: True if address is valid IPv4 address, False otherwise
+    """
+
+    regexp = "^(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"
+    if re.match(regexp, address):
+        return True
+
+    return False
+
+
+def is_ipv6(address: str) -> bool:
+    """ Checks if given address is valid IPv6 address
+    :param str address: IP address to check
+    :return bool: True if address is valid IPv6 address, False otherwise
+    """
+
+    regexp = "^(?:(?:[0-9A-Fa-f]{1,4}:){6}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|::(?:[0-9A-Fa-f]{1,4}:){5}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){4}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){3}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,2}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){2}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,3}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}:(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,4}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,5}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}|(?:(?:[0-9A-Fa-f]{1,4}:){,6}[0-9A-Fa-f]{1,4})?::)%.*$"
+
+    if re.match(regexp, address):
+        return True
+
+    return False
+
+
+def convert_ip(address: str) -> bytes:
+    """ Converts IP to bytes
+    :param str address: IP address that should be converted to bytes
+    :return bytes: IP converted to bytes format
+    """
+
+    res = b""
+    for i in address.split("."):
+        res += bytes([int(i)])
+    return res
+
+
+def convert_port(port: int) -> bytes:
+    """ Converts Port to bytes
+    :param int port: port that should be conveted to bytes
+    :return bytes: port converted to bytes format
+    """
+
+    res = "%.4x" % int(port)
+    return bytes.fromhex(res)
+
+
